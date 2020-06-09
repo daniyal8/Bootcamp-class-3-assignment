87375e816922cf666ba0779abd1982cbdab52ad2
daniyal8 <daniyalsadiq4@gmail.com>
2020-06-10 00:51:14 +0500

    intial commit

---
+++ src/Room.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import './Room.css'
+
+function Room() {
+    let [isLit, setLit] = useState(false);
+    let [Temprature , setTemprature] = useState(24);
+    
+
+    return (
+    <div className={`room ${isLit? "lit": "dark"}`}> 
+        This Room is {!isLit? "Lit": "dark"}
+        <br/>
+        Temprature: {Temprature}
+        <br/>
+        <button className="button1" onClick={()=> setLit(false) } >LightMode</button>
+        <button className="button2" onClick={()=> setLit(true) } >darkMode</button>
+        <br/>
+        <button id="button3" onClick={ ()=> {
+                        console.log("Increase age arrow function");
+                        setTemprature(++Temprature);
+                        }}>
+            +
+        </button>
+        <button id="button4" onClick={ ()=> {
+                        console.log("Increase age arrow function");
+                        setTemprature(--Temprature);
+                        }}>
+            _
+        </button>
+    </div>
+    );
+}
+
+export default Room;
